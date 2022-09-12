43058344fc6114c853bb6470428a94d3a5beaeb7
Germán Mendoza <germanmendoza03@gmail.com>
2022-09-12 18:10:46 -0300

    v 0.0.1
    Inicio de My Project

---
+++ My project/Assets/Script/Test.cs
@@ -0,0 +1,48 @@
+// using System.Diagnostics;
+// using System.Transactions;
+// using System.Threading.Tasks.Dataflow;
+// using System.Diagnostics;
+// using System.Collections;
+// using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    public int playerSpeed;
+    public int playerHealth;
+    public Vector3 playerPosition;
+    public Vector3 playerScale;
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerSpeed = 1;
+        playerHealth = 100;
+        playerScale = new Vector3(2,3,2);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        handleMove();
+    }
+
+    void handleMove()
+    {
+        transform.localScale = playerScale;
+        transform.position += ((playerPosition * Time.deltaTime ) * playerSpeed);
+    }
+
+    void damagePlayer()
+    {
+        playerHealth -= 20;
+        Debug.Log(playerHealth);
+    }
+
+    void healPlayer()
+    {
+        playerHealth += 20;
+        Debug.Log(playerHealth);
+    }
+}
